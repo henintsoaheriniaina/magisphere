b60f41ed00faafd944b9be661efa49cd794535b9
henintsoaheriniaina <belouhtsoa@gmail.com>
2025-03-07 11:40:16 +0300

    simple commit

---
+++ database/factories/AffiliationFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Affiliation>
+ */
+class AffiliationFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'label' => $this->faker->unique()->randomElement(['L1 info', 'L2 info', 'L3 info']),
+        ];
+    }
+}
---
+++ database/seeders/AffiliationSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Affiliation;
+class AffiliationSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $affiliations = [
+            ['label' => 'L1 info'],
+            ['label' => 'L2 info'],
+            ['label' => 'L3 info'],
+        ];
+
+        foreach ($affiliations as $affiliation) {
+            Affiliation::create($affiliation);
+        }
+    }
+}
